46e8f3cb0311469256b973bc9e6864a3d9f54b58
Jonas Beer <115902941+jb-cc@users.noreply.github.com>
2024-01-15 19:13:35 +0100

    changed styles

--- app/src/main/java/com/cc221012_cc221016/stash/ui/views/Composables/HomeView.kt
+++ app/src/main/java/com/cc221012_cc221016/stash/ui/views/Composables/HomeView.kt
@@ -6,67 +6,82 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material.icons.outlined.Edit
-import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeView() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "Your Stash",
-            style = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold),
-            modifier = Modifier.padding(bottom = 32.dp)
-        )
+    val colorScheme = darkColorScheme()
 
-        repeat(5) {
-            Card(
-                onClick = {},
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 16.dp)
-            ) {
-                Row(
-                    modifier = Modifier.padding(16.dp),
-                    verticalAlignment = Alignment.CenterVertically
+    MaterialTheme(colorScheme = colorScheme) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Your Stash",
+                style = MaterialTheme.typography.headlineLarge,
+                modifier = Modifier.padding(bottom = 32.dp)
+            )
+
+            repeat(5) {
+                OutlinedCard(
+                    onClick = {},
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 16.dp)
                 ) {
-                    Text(
-                        text = "Instagram",
-                        style = TextStyle(fontSize = 16.sp, fontWeight = FontWeight.Medium),
-                        modifier = Modifier.weight(1f)
-                    )
-                    Text(
-                        text = "mail@mail.com",
-                        style = TextStyle(fontSize = 14.sp)
-                    )
+                    Row(
+                        modifier = Modifier.padding(16.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            imageVector = Icons.Outlined.AccountCircle,
+                            contentDescription = "User Icon",
+                            modifier = Modifier.padding(end = 8.dp)
+                        )
+                        Column {
+                            Text(
+                                text = "Instagram",
+                                style = MaterialTheme.typography.titleMedium,
+                                modifier = Modifier.padding(bottom = 8.dp)
+                            )
+                            Text(
+                                text = "mail@mail.com",
+                                style = MaterialTheme.typography.bodyMedium,
+                            )
+                        }
+                    }
                 }
             }
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            ExtendedFloatingActionButton(
+                onClick = {},
+                icon = { Icon(Icons.Outlined.Edit, contentDescription = "Edit") },
+                text = { Text("Add entry") },
+                modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentSize(Alignment.BottomEnd)
+                    .padding(16.dp)
+            )
         }
-
-        Spacer(modifier = Modifier.weight(1f))
-
-        ExtendedFloatingActionButton(
-            onClick = {},
-            icon = { Icon(Icons.Outlined.Edit, contentDescription = "Edit") },
-            text = { Text("Add entry") },
-            modifier = Modifier.align(Alignment.CenterHorizontally)
-        )
     }
 }
--- app/src/main/java/com/cc221012_cc221016/stash/ui/views/Composables/LoginRegisterView.kt
+++ app/src/main/java/com/cc221012_cc221016/stash/ui/views/Composables/LoginRegisterView.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,12 +15,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -32,8 +31,7 @@ fun LoginRegisterView(hasUser: Boolean) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
             Text(
                 text = "Stash",
-                fontSize = 50.sp,
-                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.displayLarge,
                 modifier = Modifier.padding(top = 10.dp)
             )
 
