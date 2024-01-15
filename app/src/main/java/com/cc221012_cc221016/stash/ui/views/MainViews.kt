25b572adc1a85176a4a287dffbadc8891de69234
Jonas Beer <115902941+jb-cc@users.noreply.github.com>
2024-01-15 22:02:54 +0100

    optimized imports

--- app/src/main/java/com/cc221012_cc221016/stash/ui/views/MainViews.kt
+++ app/src/main/java/com/cc221012_cc221016/stash/ui/views/MainViews.kt
@@ -1,11 +1,8 @@
 package com.cc221012_cc221016.stash.ui.views
 
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.ViewModel
 import com.cc221012_cc221016.stash.ui.views.Composables.AddEntryView
-import com.cc221012_cc221016.stash.ui.views.Composables.LoginRegisterView
-import com.cc221012_cc221016.stash.ui.views.Composables.HomeView
 
 class MainViewModel : ViewModel() // Dummy ViewModel
 
@@ -14,7 +11,6 @@ sealed class Screen(val route: String){
     object Second: Screen("second")
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainView(mainViewModel: MainViewModel){
     //    LoginRegisterView(hasUser = false)
