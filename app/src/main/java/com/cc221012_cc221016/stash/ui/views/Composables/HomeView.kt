88a0fff440fa1b2673d2bfa24dd8e0d5e1668a9a
Jonas Beer <115902941+jb-cc@users.noreply.github.com>
2024-01-15 20:11:24 +0100

    changed structure of HomeView, added spacing at the end to ensure readability

--- app/src/main/java/com/cc221012_cc221016/stash/ui/views/Composables/HomeView.kt
+++ app/src/main/java/com/cc221012_cc221016/stash/ui/views/Composables/HomeView.kt
@@ -1,12 +1,14 @@
 package com.cc221012_cc221016.stash.ui.views.Composables
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material.icons.outlined.Edit
@@ -28,58 +30,61 @@ fun HomeView() {
     val colorScheme = darkColorScheme()
 
     MaterialTheme(colorScheme = colorScheme) {
-        Column(
+        Box(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+                .padding(16.dp)
         ) {
-            Text(
-                text = "Your Stash",
-                style = MaterialTheme.typography.headlineLarge,
-                modifier = Modifier.padding(bottom = 32.dp)
-            )
+            Column {
+                Text(
+                    text = "Your Stash",
+                    style = MaterialTheme.typography.headlineLarge,
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
 
-            repeat(5) {
-                OutlinedCard(
-                    onClick = {},
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = 16.dp)
-                ) {
-                    Row(
-                        modifier = Modifier.padding(16.dp),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Icon(
-                            imageVector = Icons.Outlined.AccountCircle,
-                            contentDescription = "User Icon",
-                            modifier = Modifier.padding(end = 8.dp)
-                        )
-                        Column {
-                            Text(
-                                text = "Instagram",
-                                style = MaterialTheme.typography.titleMedium,
-                                modifier = Modifier.padding(bottom = 8.dp)
-                            )
-                            Text(
-                                text = "mail@mail.com",
-                                style = MaterialTheme.typography.bodyMedium,
-                            )
+                LazyColumn {
+                    items(20) {
+                        OutlinedCard(
+                            onClick = {},
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(bottom = 16.dp)
+                        ) {
+                            Row(
+                                modifier = Modifier.padding(16.dp),
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Outlined.AccountCircle,
+                                    contentDescription = "User Icon",
+                                    modifier = Modifier.padding(end = 8.dp)
+                                )
+                                Column {
+                                    Text(
+                                        text = "Instagram",
+                                        style = MaterialTheme.typography.titleMedium,
+                                        modifier = Modifier.padding(bottom = 8.dp)
+                                    )
+                                    Text(
+                                        text = "mail@mail.com",
+                                        style = MaterialTheme.typography.bodyMedium,
+                                    )
+                                }
+                            }
                         }
+                    }
+                    item {
+                        Spacer(modifier = Modifier.height(60.dp))
                     }
                 }
             }
-
-            Spacer(modifier = Modifier.weight(1f))
 
             ExtendedFloatingActionButton(
                 onClick = {},
                 icon = { Icon(Icons.Outlined.Edit, contentDescription = "Edit") },
                 text = { Text("Add entry") },
                 modifier = Modifier
-                    .fillMaxSize()
-                    .wrapContentSize(Alignment.BottomEnd)
+                    .align(Alignment.BottomEnd)
                     .padding(16.dp)
             )
         }
